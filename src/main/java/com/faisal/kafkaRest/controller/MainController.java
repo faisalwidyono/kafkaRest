8d14111f272635e145d00910007f2cd6291e0e19
faisalwidyono <fwidyono@gmail.com>
2019-07-22 18:43:21 +0700

    Update Kafka Consumer

--- src/main/java/com/faisal/kafkaRest/controller/MainController.java
+++ src/main/java/com/faisal/kafkaRest/controller/MainController.java
@@ -3,25 +3,23 @@ package com.faisal.kafkaRest.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faisal.kafkaRest.dao.TopicDAO;
 import com.faisal.kafkaRest.dto.TopicDTO;
 import com.faisal.kafkaRest.model.Topic;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 
 
+@Api(tags = "Producer")
 @RestController
 @RequestMapping(value = "api/v1")
 public class MainController {
@@ -39,7 +37,12 @@ public class MainController {
 		topicDAO.findAll().forEach(result::add);
 		return result;
 	}
-
+	
+	@ApiOperation(
+			value = "API to publish data",
+		    notes = "Publish data to APache Kafka",
+		    tags = "Publish Data"
+			)
 	@PostMapping(value = "/publish")
 	public TopicDTO publish(@RequestBody TopicDTO topicDTO) {
 
